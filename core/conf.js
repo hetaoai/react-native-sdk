53888274013f0f64dbb37c140d668bdd29fea08b
Xiaoxu Yang <xyang@hetao.ai>
2019-03-06 14:53:39 +0800

    update url

--- core/conf.js
+++ core/conf.js
@@ -4,12 +4,12 @@
 let ACCESS_KEY = '<YOU AK>';
 let SECRET_KEY = '<YOU SK>';
 
-let UP_HOST = 'https://upload.qiniu.com';
+let UP_HOST = 'https://up-z1.qiniup.com';
 let RS_HOST = 'https://rs.qbox.me';
 let RSF_HOST = 'https://rsf.qbox.me';
 let API_HOST = 'https://api.qiniu.com';
 let RPC_TIMEOUT = 3600000; // default rpc timeout: one hour
 
 export default {
-  ACCESS_KEY,SECRET_KEY,UP_HOST,RS_HOST,RSF_HOST,API_HOST,RPC_TIMEOUT
+    ACCESS_KEY, SECRET_KEY, UP_HOST, RS_HOST, RSF_HOST, API_HOST, RPC_TIMEOUT
 }
